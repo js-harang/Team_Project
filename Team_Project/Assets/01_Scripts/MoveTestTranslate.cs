57dc6a01da801d395152e2971bb5b6434c14ffd8
ShieldTanker <eogus2346@naver.com>
2024-06-03 15:58:51 +0900

    Move움직임 Translate 추가

---
+++ Team_Project/Assets/01_Scripts/MoveTestTranslate.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTestTranslate : MonoBehaviour
+{
+    // Translate Å×½ºÆ®
+    public float speed;
+
+    private void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = new Vector3(moveX, 0, moveZ);
+
+        transform.Translate(move * speed * Time.deltaTime);
+    }
+}
