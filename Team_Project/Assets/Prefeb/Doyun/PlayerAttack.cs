64085fb00f6ebef132115dfbca6772bb55078a3d
ShieldTanker <eogus2346@naver.com>
2024-06-07 16:00:09 +0900

    플레이어 공격 방식 변경중
    전략패턴 을 사용하여 공격 하나하나 스크립트 로 하여 기본공격 과 스킬 공격을 교체하여 사용할수있게 변경중

---
+++ Team_Project/Assets/Prefeb/Doyun/PlayerAttack.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct AttackStatus
+{
+    // 공격력
+    public int atkPower;
+    // 공격 딜레이
+    public float atkDelay;
+    // 공격 범위
+    public Vector3 atkLenght;
+
+    // 소모 자원
+    public int useMana;
+}
+
+public abstract class PlayerAttack : MonoBehaviour
+{
+    public AttackStatus aST;
+
+    [SerializeField]
+    Collider[] enemys;
+
+    public abstract void InitSetting();
+
+    public virtual void Attack(Transform atkPosition)
+    {
+        Vector3 position = atkPosition.transform.position;
+        enemys = Physics.OverlapBox(position, aST.atkLenght, Quaternion.identity);
+
+        foreach (Collider enemy in enemys)
+        {
+            if (enemy.gameObject.tag == "Enemy")
+            {
+
+                Debug.Log("적 공격 데미지 :" + aST.atkPower);
+            }
+        }
+    }
+}
