1acfd5f8537f6306eb263d744b49efd2e9c30e38
chw0827 <92154256+chw0827@users.noreply.github.com>
2024-06-11 16:29:11 +0900

    1

--- Team_Project/Assets/01_Scripts/GoBattleStage.cs
+++ Team_Project/Assets/01_Scripts/GoBattleStage.cs
@@ -26,6 +26,7 @@ public class GoBattleStage : MonoBehaviour
 
     public void GoBattle()
     {
-        SceneManager.LoadScene("04_BattleStage");
+        GameManager.gm.sceneNumber = 4;
+        SceneManager.LoadScene("99_LoadingScene");
     }
 }
