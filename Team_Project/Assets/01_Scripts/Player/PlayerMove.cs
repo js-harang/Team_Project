1f70aafe919ab34c401b424b0c1fb09984a8f408
js-harang <harang3427@naver.com>
2024-06-12 12:47:01 +0900

    2024-06-12
    작업 시작

--- Team_Project/Assets/01_Scripts/Player/PlayerMove.cs
+++ Team_Project/Assets/01_Scripts/Player/PlayerMove.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 
@@ -15,7 +16,7 @@ public class PlayerMove : MonoBehaviour
     float inputZ;
 
     // 이동
-    public float setSpeed;
+    [SerializeField]
     float speed;
     public float jumpForce;
 
@@ -31,10 +32,13 @@ public class PlayerMove : MonoBehaviour
 
     public GameObject playerPointer;
 
+    CapsuleCollider playerCollider;
 
     private void Start()
     {
         StartSetting();
+
+        playerCollider = GetComponent<CapsuleCollider>();
     }
 
     private void Update()
@@ -47,11 +51,12 @@ public class PlayerMove : MonoBehaviour
 
     }
 
-    void InputKey()
-    {   // 키입력
+    private void InputKey()
+    {
+        // 키입력
         if (pState.UnitState == UnitState.Interact)
             return;
-        
+
         inputX = Input.GetAxisRaw("Horizontal");
         inputZ = Input.GetAxisRaw("Vertical");
 
@@ -60,8 +65,9 @@ public class PlayerMove : MonoBehaviour
         Jump();
     }
 
-    void Move()
-    {   // 아무 이동키 입력시
+    private void Move()
+    {
+        // 아무 이동키 입력시
         if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
         {
             if (isGround)
@@ -70,9 +76,11 @@ public class PlayerMove : MonoBehaviour
                 anim.SetTrigger("IdleToMove");
             }
 
-            speed = setSpeed;
-
             Vector3 inputMove = new Vector3(inputX, 0, inputZ).normalized;
+
+            // 벽 충돌
+            if (CheckHitWall(inputMove))
+                return;
 
             Vector3 move = transform.position + (inputMove * speed * Time.deltaTime);
             transform.position = move;
@@ -85,16 +93,16 @@ public class PlayerMove : MonoBehaviour
                 anim.SetTrigger("MoveToIdle");
             }
 
-            speed = 0f;
+            //speed = 0f;
         }
     }
 
-    void GrondCheck()
-    {    // 바닥 체크용 레이
-        Ray ray = new Ray(transform.position, Vector3.down);
-        RaycastHit hitInfo;
+    private void GrondCheck()
+    {
+        // 바닥 체크용 레이
+        Ray ray = new(transform.position, Vector3.down);
 
-        if (Physics.Raycast(ray, out hitInfo, checkDis, grdLayer))
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, checkDis, grdLayer))
         {
             isGround = true;
             anim.SetBool("IsGround", isGround);
@@ -107,23 +115,23 @@ public class PlayerMove : MonoBehaviour
         DrawArrowToGround();
     }
 
-    void DrawArrowToGround()
+    private void DrawArrowToGround()
     {
-        Ray ray = new Ray(transform.position, Vector3.down);
-        RaycastHit hitInfo;
+        Ray ray = new(transform.position, Vector3.down);
 
-        if (Physics.Raycast(ray, out hitInfo, grdLayer))
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, grdLayer))
         {
             if (!isGround)
             {
                 playerPointer.SetActive(true);
 
                 playerPointer.transform.position = hitInfo.point;
-            /*        new Vector3(
-                    hitInfo.transform.position.x,
-                    hitInfo.transform.position.y + 1,
-                    hitInfo.transform.position.z);
-            */}
+                /*        new Vector3(
+                        hitInfo.transform.position.x,
+                        hitInfo.transform.position.y + 1,
+                        hitInfo.transform.position.z);
+                */
+            }
             else
             {
                 playerPointer.SetActive(false);
@@ -131,8 +139,9 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
-    void Jump()
-    {        // 점프키 입력시
+    private void Jump()
+    {
+        // 점프키 입력시
         if (Input.GetKeyDown(KeyCode.Space) && isGround)
         {
             pState.UnitState = UnitState.Jump;
@@ -141,8 +150,9 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
-    void LookDirection()
-    {   // 움직임 상태일때 움직이는 방향으로 회전
+    private void LookDirection()
+    {
+        // 움직임 상태일때 움직이는 방향으로 회전
         if (Input.GetButton("Horizontal"))
         {
             if (inputX != 0)
@@ -153,8 +163,9 @@ public class PlayerMove : MonoBehaviour
         transform.rotation = Quaternion.LookRotation(lookdir);
     }
 
-    void StartSetting()
-    {   // Start() 에서 사용할 기능들 모음
+    private void StartSetting()
+    {
+        // Start() 에서 사용할 기능들 모음
         rb = GetComponent<Rigidbody>();
         lookInput = 1f;
         transform.rotation = Quaternion.LookRotation(Vector3.right);
@@ -167,8 +178,39 @@ public class PlayerMove : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
-        Gizmos.DrawLine(transform.position,transform.position + Vector3.down * checkDis);
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * checkDis);
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position,transform.position + Vector3.down * 100f);
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * 100f);
+    }
+
+    /// <summary>
+    /// 벽 통과 방지 메서드
+    /// </summary>
+    /// <param name="inputmove"></param>
+    /// <returns></returns>
+    private bool CheckHitWall(Vector3 inputmove)
+    {
+        inputmove = transform.TransformDirection(inputmove);
+        float scope = 1f;
+
+        // 플레이어의 머리, 가슴, 발 총 3군데에서 ray를 쏜다
+        List<Vector3> rayPositions = new()
+        {
+            transform.position + Vector3.up * 0.1f,
+            transform.position + Vector3.up * playerCollider.height * 0.5f,
+            transform.position + Vector3.up * playerCollider.height
+        };
+
+        // 충돌 체크
+        foreach (Vector3 pos in rayPositions)
+        {
+            if (Physics.Raycast(pos, inputmove, out RaycastHit hit, scope))
+            {
+                if (hit.collider.gameObject.layer == 7)
+                    return true;
+            }
+        }
+
+        return false;
     }
 }
