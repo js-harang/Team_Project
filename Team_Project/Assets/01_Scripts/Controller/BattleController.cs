81d1f8744dd7a2dc48192582ef5953603100e7c9
chw0827 <92154256+chw0827@users.noreply.github.com>
2024-06-19 17:30:28 +0900

    진행 저장

--- Team_Project/Assets/01_Scripts/Controller/BattleController.cs
+++ Team_Project/Assets/01_Scripts/Controller/BattleController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public enum BattleState
 {
@@ -18,7 +19,19 @@ public class BattleController : MonoBehaviour
 {
     [SerializeField]
     BattleState battleSate;
-    public BattleState BattleState { get { return battleSate; } set { battleSate = value; } }
+    public BattleState BattleState 
+    { 
+        get 
+        { 
+            return battleSate; 
+        } 
+        set 
+        { 
+            battleSate = value;
+            if (value == BattleState.BattleEnd)
+                BattleEndProcess();
+        } 
+    }
 
     PlayerState pS;
     [SerializeField]
@@ -37,6 +50,12 @@ public class BattleController : MonoBehaviour
 
     public GameObject battleStartImg;
     public GameObject battleClearUI;
+    public GameObject gameOverUI;
+    public GameObject battleEndUI;
+
+    public GameObject battleShopNPC;
+    public Transform EndShopSpawnPosition;
+
 
     private void Start()
     {
@@ -45,6 +64,9 @@ public class BattleController : MonoBehaviour
 
     private void Update()
     {
+        if (pS.UnitState == UnitState.Die)
+            battleSate = BattleState.Defeat;
+
         BattleStageProcess(battleSate);
     }
 
@@ -69,6 +91,7 @@ public class BattleController : MonoBehaviour
                 StartCoroutine(ClearProcess());
                 break;
             case BattleState.Defeat:
+                GameOver();
                 break;
             case BattleState.BattleEnd:
                 break;
@@ -81,9 +104,11 @@ public class BattleController : MonoBehaviour
     IEnumerator BattleIntro()
     {
         pS.UnitState = UnitState.Interact;
+
         yield return new WaitForSeconds(3f);
-        pS.UnitState = UnitState.Idle;
+
         battleSate = BattleState.Start;
+        pS.UnitState = UnitState.Idle;
     }
 
     // 배틀 시작시 Start 이미지 애니메이션 재생
@@ -92,19 +117,49 @@ public class BattleController : MonoBehaviour
         Animator startAnim = battleStartImg.GetComponent<Animator>();
         startAnim.SetTrigger("nowStart");
         battleSate = BattleState.Running;
+
         yield return new WaitForSeconds(2f);
+
         battleStartImg.SetActive(false);
     }
 
+    // 배틀 클리어시 작동하는 과정
     IEnumerator ClearProcess()
     {
         pS.UnitState = UnitState.Interact;
         battleClearUI.SetActive(true);
         Animator clearAnim = battleClearUI.GetComponent<Animator>();
         clearAnim.SetTrigger("stageClear");
+
         yield return new WaitForSeconds(3f);
+
         battleClearUI.SetActive(false);
         battleSate = BattleState.BattleEnd;
         pS.UnitState = UnitState.Idle;
+    }
+
+    // 캐릭터 사밍시 버튼 누르면 마을로
+    public void ToTown()
+    {
+        GameManager.gm.sceneNumber = 2;
+        SceneManager.LoadScene("99_LoadingScene");
+    }
+
+    // 게임오버시 호출되는 메소드(게임오버 매뉴 활성화)
+    void GameOver()
+    {
+        gameOverUI.SetActive(true);
+    }
+
+    void BattleEndProcess()
+    {
+        Instantiate(battleShopNPC, EndShopSpawnPosition);
+        battleEndUI.SetActive(true);
+    }
+
+    public void OnemoreTry()
+    {
+        GameManager.gm.sceneNumber = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene("99_LoadingScene");
     }
 }
