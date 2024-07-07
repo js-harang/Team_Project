4919fa3ca433ec2679b50ae7cec39a9d6a40e4d4
js-harang <harang3427@naver.com>
2024-07-08 01:42:44 +0900

    database online or local function add

--- Team_Project/Assets/01_Scripts/Controller/SceneController/TitleController.cs
+++ Team_Project/Assets/01_Scripts/Controller/SceneController/TitleController.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
+public enum Network
+{
+    Online,
+    Offline,
+}
+
 public class TitleController : MonoBehaviour
 {
-    [SerializeField] TMP_InputField id;
+    [SerializeField] Network network;
+
+    [SerializeField, Space(10)] TMP_InputField id;
     [SerializeField] TMP_InputField pw;
     [SerializeField] TextMeshProUGUI loginResultTxt;
     [SerializeField] TextMeshProUGUI idPlaceholder;
@@ -34,17 +42,48 @@ public class TitleController : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// 로그인
-    /// </summary>
-    public void LoginCheck()
+    public void LoginBtn()
     {
         if (!CheckInput(id.text, pw.text))
             return;
 
-        StartCoroutine(LoginDataPost(id.text, pw.text));
+        switch (network)
+        {
+            case Network.Online:
+                StartCoroutine(LoginDataPost(id.text, pw.text));
+                break;
+            case Network.Offline:
+                LoginDataPlayerPrefs();
+                break;
+        }
     }
 
+    public void SaveDataBtn()
+    {
+        if (!CheckInput(id.text, pw.text))
+            return;
+
+        if (id.text.Length > 20)
+        {
+            loginResultTxt.text = "아이디는 20자까지 입력할 수 있습니다.";
+            return;
+        }
+
+        switch (network)
+        {
+            case Network.Online:
+                StartCoroutine(SaveDataPost(id.text, pw.text));
+                break;
+            case Network.Offline:
+                SaveDataPlayerPrefs();
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 데이터베이스 온라인용
+    /// </summary>
+    #region
     IEnumerator LoginDataPost(string id, string pw)
     {
         string url = path + "login.php";
@@ -66,20 +105,6 @@ public class TitleController : MonoBehaviour
                         break;
                 }
         }
-    }
-
-    public void SaveUserData()
-    {
-        if (!CheckInput(id.text, pw.text))
-            return;
-
-        if(id.text.Length > 20)
-        {
-            loginResultTxt.text = "아이디는 20자까지 입력할 수 있습니다.";
-            return;
-        }
-
-        StartCoroutine(SaveDataPost(id.text, pw.text));
     }
 
     IEnumerator SaveDataPost(string id, string pw)
@@ -104,6 +129,40 @@ public class TitleController : MonoBehaviour
                 }
         }
     }
+    #endregion
+
+    /// <summary>
+    /// 데이터베이스 오프라인용
+    /// </summary>
+    #region
+    private void LoginDataPlayerPrefs()
+    {
+        if (!PlayerPrefs.HasKey(id.text))
+        {
+            loginResultTxt.text = "입력하신 아이디와 패스워드가 일치하지 않습니다.";
+            return;
+        }
+
+        if (pw.text == PlayerPrefs.GetString(id.text))
+        {
+            GameManager.gm.sceneNumber = 1;
+            SceneManager.LoadScene("99_LoadingScene");
+        }
+        else
+            loginResultTxt.text = "입력하신 아이디와 패스워드가 일치하지 않습니다.";
+    }
+
+    private void SaveDataPlayerPrefs()
+    {
+        if (!PlayerPrefs.HasKey(id.text))
+        {
+            PlayerPrefs.SetString(id.text, pw.text);
+            loginResultTxt.text = "아이디 생성이 완료됐습니다.";
+        }
+        else
+            loginResultTxt.text = "이미 존재하는 아이디입니다.";
+    }
+    #endregion
 
     private bool CheckInput(string id, string pw)
     {
