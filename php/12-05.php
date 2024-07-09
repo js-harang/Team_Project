e4349e7cb86584bae552cfee9721f2aa35c1f688
js-harang <harang3427@naver.com>
2024-07-09 11:48:01 +0900

    php

--- php/12-05.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$str1 = "이것이 MariaDB다<br>"; echo $str1;
-	$str2 = 'PHP 프로그래밍<br>";		echo $str2;
-	$str3 = "select * from userTBL where userID='JYP'"; echo $str3;
-?>
--- php/info.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db = mysqli_connect("localhost", "root", "1234", "madang");
-if ($db)
-	echo "connect : success<br>";
-else
-	echo "connect : failure<br>";
-?>
