02f94294547796f831a11a30598954a77b39819c
ShieldTanker <eogus2346@naver.com>
2024-07-12 16:14:11 +0900

    123123

--- Team_Project/Assets/01_Scripts/UI/Skill/CheckDuplication.cs
+++ Team_Project/Assets/01_Scripts/UI/Skill/CheckDuplication.cs
@@ -6,23 +6,25 @@ using UnityEngine.UI;
 public class CheckDuplication : MonoBehaviour
 {
     public SkillButton[] btns;
-
-    public bool CheckSkillDuplication(int btnIdx,int idx)
+    /// <summary>
+    /// 모든 버튼에 등록할 스킬번호 가 있는지 조회
+    /// </summary>
+    /// <param name="btnIdx"></param>
+    /// <param name="idx"></param>
+    /// <returns></returns>
+    public bool CheckSkillDuplication(int idx)
     {
         bool isDuplication = false;
 
         for (int i = 0; i < btns.Length; i++)
         {
-            if (btnIdx != i)
+            if (btns[i].Skill != null)
             {
-                if (btns[i].Skill != null)
+                // 등록된 버튼들 의 스킬의 번호와 현재 버튼의 스킬번호와 같으면
+                if (btns[i].Skill.skillIdx == idx)
                 {
-                    // 등록된 버튼의 스킬의 번호와 현재 버튼의 스킬번호와 같으면
-                    if (btns[i].Skill.skillIdx == idx)
-                    {
-                        isDuplication = true;
-                        return isDuplication;
-                    }
+                    isDuplication = true;
+                    return isDuplication;
                 }
             }
         }
