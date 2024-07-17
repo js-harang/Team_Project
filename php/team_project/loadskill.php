303036b7ca969b93fdfb6e3decbffb402596d577
js-harang <harang3427@naver.com>
2024-07-17 16:34:29 +0900

    Update loadskill.php

--- php/team_project/loadskill.php
+++ php/team_project/loadskill.php
@@ -4,21 +4,21 @@ $username = "root";
 $password = "1234";
 $dbname = "team_project";
 
-$cuid = $_POST["cuid"];
-$skill = $_POST["skill"];
+$cuid = $_GET["cuid"];
+$skill = $_GET["skill"];
 $tbl = "character_info";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
-    echo "Failed to connect to MySQL : " + $mysqli->connect_error;}
+	echo "Failed to connect to MySQL : " + $mysqli->connect_error;
+}
 
-$sql = "SELECT '".$skill."'
-		FROM	'".$tbl."'
-		WHERE	character_uid = '".$cuid."'";
-
+$sql = "select	$skill
+		from	$tbl
+		where	character_uid = '0000000013'";
 $result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_assoc($resert);
-echo $row['".$skill."'];
+$row = mysqli_fetch_assoc($result);
+echo $row['skill_0'];
 
 return;
 ?>
