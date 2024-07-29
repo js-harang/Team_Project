1087695dfbb2ee21aaa2fba457ba015473a0a2cb
chw0827 <92154256+chw0827@users.noreply.github.com>
2024-07-29 11:01:30 +0900

    1

--- php/team_project/load_playerquestsdata.php
+++ php/team_project/load_playerquestsdata.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "1234";
 $dbname = "team_project";
 
-$cuid = $_POST["cuid"];
+$questID = $_POST["questID"];
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -12,6 +12,25 @@ if ($conn->connect_error) {
 	echo "Failed to connect to MySQL : " + $mysqli->connect_error;
 }
 
+$sql = "SELECT 	* 
+		FROM 	questdata 
+		WHERE 	quest_id = $questID";
+		
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+	
+	$quest = 'quest_name' => $row['quest_name'],
+			  'quest_id' => $row['quest_id'],
+			  'giver_id' => $row['giver_id'],
+			  'target_type' => $row['target_type'],
+			  'target_id' => $row['target_id'],
+			  'quest_goalline' => $row['quest_goalline'],
+			  'gold_reward' => $row['gold_reward'],
+			  'exp_reward' => $row['exp_reward']
+			
+	$quest_json_string = json_encode($quest, JSON_UNESCAPED_UNICODE);
+	echo $quest_json_string;
+}
 
 mysqli_close($conn);
 ?>
